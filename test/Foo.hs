1df54340edc8d9104015ca47816c72616378d546
Hans Höglund <hans@hanshoglund.se>
2012-11-15 22:34:49 +0100

    - Tests

--- test/Foo.hs
+++ test/Foo.hs
@@ -1,19 +1,18 @@
 
 module Foo where
 
-one = 1
-name = "hans"
-char = 'c'
+-- A comment
+-- | This is one.
+-- one = 1
+-- two = 2
+-- name = "hans"
+-- char = 'c'
 
-id x    = x
-const x = \y -> x
+-- id x      = x
+-- const x   = \y -> x
+-- app x y z = x z (y z) 
+-- nest = \x -> \y -> \z -> x + y + z
 
-app  f x    = f x
-app2 f x y  = f x y
 
-foo x y = x + (succ $ y)
-bar     = 1 + (2 * 3)
 
-stdVarName = 1
-quiteLongName = 1
-thisIsAVeryLongBaritoneVariableName = 1
+foo = \x -> x + a + b + c
