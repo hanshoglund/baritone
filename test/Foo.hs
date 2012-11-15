5f33e90a99c6c71cd67e75c0ddadfc625ecf740c
Hans Höglund <hans@hanshoglund.se>
2012-11-15 22:59:20 +0100

    - More tests

--- test/Foo.hs
+++ test/Foo.hs
@@ -3,16 +3,14 @@ module Foo where
 
 -- A comment
 -- | This is one.
--- one = 1
--- two = 2
--- name = "hans"
--- char = 'c'
+one = 1
+two = 2
+name = "hans"
+char = 'c'
 
--- id x      = x
--- const x   = \y -> x
--- app x y z = x z (y z) 
--- nest = \x -> \y -> \z -> x + y + z
+id x      = x
+const x   = \y -> x
+app x y z = x z (y z) 
+nest      = \x -> \y -> \z -> x + y + z
 
-
-
-foo = \x -> x + a + b + c
+foo       = \x -> x + a + b + c
