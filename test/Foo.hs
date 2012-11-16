a20ba24954ac53c26850597318c7ac4a4e2d9ede
Hans Höglund <hans@hanshoglund.se>
2012-11-16 13:02:48 +0100

    - Tests

--- test/Foo.hs
+++ test/Foo.hs
@@ -7,10 +7,15 @@ one = 1
 two = 2
 name = "hans"
 char = 'c'
+add_    = 0
+zipWith = 0
+if_     = 0
 
-id x      = x
-const x   = \y -> x
-app x y z = x z (y z) 
-nest      = \x -> \y -> \z -> x + y + z
+id x     = x
+const x  = \y -> x
+const2 x = \y -> \z -> x
 
-foo       = \x -> x + a + b + c
+adder a b = \x -> (a*x) + (b*x)
+fibs = 0 `cons` 1 `cons` zipWith plus fibs (tail fibs)
+
+map f xs = if_ (isNull xs) null (cons (f `ap` head xs) (map f `ap` tail xs))
