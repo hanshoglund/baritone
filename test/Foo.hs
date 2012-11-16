f8bbc0207da4ff425003ccdf4d2df24b8d7e42b9
Hans Höglund <hans@hanshoglund.se>
2012-11-17 00:18:53 +0100

    - More tests

--- test/Foo.hs
+++ test/Foo.hs
@@ -3,27 +3,44 @@ module Foo.Bar.Baz where
 
 -- A comment
 -- | This is one.
-__neg a     = inline "-%" x
-__add a b   = inline "% + %" a b
-__mul a b   = inline "% * %" a b
+__trace a = inline ["trace(x)", a]
+__neg a   = inline ["-x", a]
+__add a b = inline ["x + x", a, b]
+__mul a b = inline ["x * x", a, b]
+__sub a b = inline ["x - x", a, b]
+__div a b = inline ["x / x", a, b]
+
+__foreach f as = inline ["for each v in x { x.A(v) }", as, f]
+__c2 a b   = inline ["CreateSparseArray(x,x)", a, b]
+__g1 a     = inline ["x[0]", a]
+__g2 a     = inline ["x[1]", a]
+
+
+
+
 
 one = 1
 two = 2
 name = "hans"
 char = 'c'       
-negOne = (-1)
+-- negOne = (-1)
 
 id x          = x
 const x y     = x
 const2 x y z  = x
 
-comp1 f g x = f (g x)
+comp f g x = f (g x)
 
+pred x = x - 1
+succ x = x + 1
 adder a b x = (a*x) + (b*x)
 add23 = adder 2 3
+pair = __c2 555 666
 
-main = trace (add23 5)
-
+main  = __trace (add23 10)
+main1 = __trace ((comp succ succ) 0)
+main2 = __trace (__g1 pair)
+main3 = __trace (__g2 pair)
 
 
 -- f = (a b c)  (\x -> a b x (c d x)) (\y -> \z -> m n y z)
